c85ffb2481c6c8ab8b78c70bcbd3e276bd98dd56
Anastiuk2412 <dse6023@gmail.com>
2021-10-13 20:48:10 +0300

    addIndexFix

--- database/migrations/20211007181421_users.js
+++ database/migrations/20211007181421_users.js
@@ -3,7 +3,7 @@ export const up = (knex) => {
     table.increments('id');
     table.string('name').notNullable();
     table.string('surname').notNullable();
-    table.string('email').notNullable();
+    table.string('email').notNullable().unique();
     table.string('password').notNullable();
     table.boolean('is_active').notNullable();
     table.string('confirm_user').notNullable();
