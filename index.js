6835b85105178ae5057547ec836bd7aee134bfc9
Anastiuk2412 <dse6023@gmail.com>
2021-10-20 16:34:31 +0300

    loginRegisterValidate

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ import express from 'express';
 import config from 'config';
 import bodyParser from 'body-parser';
 import { login, register } from './app/Http/Controllers/user.js';
+import { registerValidate } from './lib/validate/register.js';
+import { loginValidate } from './lib/validate/login.js';
+import { validationResult } from 'express-validator';
 
 const PORT = config.get('PORT');
 
@@ -9,10 +12,18 @@ const app = express();
 app.use(bodyParser.json());
 
 /* Routers Auth */
-app.get('/user/login', (req, res) => {
+app.get('/user/login', loginValidate, (req, res) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
   login(req, res);
 });
-app.post('/user/registration', (req, res) => {
+app.post('/user/registration', registerValidate, (req, res) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
   register(req, res);
 });
 
