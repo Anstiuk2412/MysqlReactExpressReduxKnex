81afe5d8cf0ce19d43ffb6b5db10a0fe74cd5f55
Anastiuk2412 <dse6023@gmail.com>
2021-10-21 12:24:23 +0300

    merged and resolved the conflict in knexfile.js

--- database/migrations/20211019160015_emailIndex.js
+++
@@ -1,9 +0,0 @@
-export const up = (knex) => {
-  return knex.schema.table('users', (table) => {
-    table.unique(['email']);
-  });
-};
-
-export const down = (knex) => {
-  return knex.schema.dropUnique('email', 'email');
-};
