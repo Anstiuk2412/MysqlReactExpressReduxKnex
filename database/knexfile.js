5939964f7d43a609e2b0915e502356c5781bca31
Anastiuk2412 <dse6023@gmail.com>
2021-10-21 14:03:24 +0300

    Revert "addMigrateMailIndex"
    This reverts commit 2a13fc70

--- database/knexfile.js
+++ database/knexfile.js
@@ -1,22 +1,21 @@
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
 import * as path from 'path';
-
 dotenv.config({ path: path.resolve('./.env') });
 
-const config = {
-  development: {
-    client: 'mysql2',
-    connection: {
-      host: process.env.DB_HOST,
-      port: process.env.DB_PORT,
-      user: process.env.DB_USER,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_DATABASE,
-    },
-    migrations: {
-      directory: './migrations',
-      loadExtensions: ['.js'],
-    },
-  },
+const config ={
+    development: {
+        client: 'mysql2',
+        connection: {
+            host: process.env.DB_HOST,
+            port: process.env.DB_PORT,
+            user: process.env.DB_USER,
+            password: process.env.DB_PASSWORD,
+            database: process.env.DB_DATABASE
+        },
+        migrations: {
+            directory: './migrations',
+            loadExtensions: ['.js']
+        }
+    }
 };
 export default config;
