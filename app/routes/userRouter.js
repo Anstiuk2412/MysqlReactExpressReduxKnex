e2d72b5c685ffc9fd694290ebb39557b7fe20fe5
Anastiuk <dse6023@gmail.com>
2021-11-08 17:16:06 +0200

    createValidation

--- app/routes/userRouter.js
+++ app/routes/userRouter.js
@@ -1,11 +1,16 @@
 import passport from 'passport';
 import { login, register } from '../Http/Controllers/user.js';
 import { Router } from 'express';
+import {
+  registerValidate,
+  loginValidate,
+  validate,
+} from '../../lib/validate/user.js';
 
 export const router = Router();
 
 router.get('/', passport.authenticate('jwt', { session: false }), (req, res) =>
   res.send('HOME'),
 );
-router.get('/user/login', login);
-router.post('/user/registration', register);
+router.get('/user/login', loginValidate, validate, login);
+router.post('/user/registration', registerValidate, validate, register);
