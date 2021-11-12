4848340c2cf28c1ccc52e37726bdd9e1cef352f3
Anastiuk <dse6023@gmail.com>
2021-11-12 13:30:25 +0200

    refixTestPassportFn

--- test/libPassport/index.test.js
+++ test/libPassport/index.test.js
@@ -3,25 +3,23 @@ import sinon from 'sinon';
 import { user } from '../../database/models/user.js';
 import { verify } from '../../lib/passport/passport.js';
 
-describe('user db tests', () => {
+describe('User db passport tests', () => {
+  const done = (val, value) => {
+    return value;
+  };
   afterEach(() => {
     sinon.restore();
   });
-  it('should return non-existed user', async () => {
+  it('Should return non-existed user', async () => {
     sinon.stub(user, 'selectFirst').resolves(false);
-    const done = (val, value) => {
-      return value;
-    };
     const check = await verify('', done);
     expect(check).to.be.false;
   });
 
-  it('should return existed user', async () => {
+  it('Should return existed user', async () => {
     const fakeUser = { id: 1 };
     sinon.stub(user, 'selectFirst').resolves(fakeUser);
-    const done = (val, value) => {
-      return value;
-    };
+
     const check = await verify('', done);
     expect(check).to.be.deep.equal({ id: 1 });
   });
