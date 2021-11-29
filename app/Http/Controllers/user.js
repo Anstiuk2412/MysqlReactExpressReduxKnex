2e3b20aca84b25706f4fee44664e0482f1cdc807
Anastiuk <dse6023@gmail.com>
2021-11-29 18:47:18 +0200

    Backend add to Controller fn sendFile() and logout(). Add routes for  JS files,  and logout

--- app/Http/Controllers/user.js
+++ app/Http/Controllers/user.js
@@ -1,57 +1,69 @@
 import {
-  comparePass,
-  createHash,
-  createToken,
+    comparePass,
+    createHash,
+    createToken,
 } from '../../../lib/auth/auth.js';
-import { user } from '../../../database/models/user.js';
+import {user} from '../../../database/models/user.js';
+import {fileURLToPath} from 'url';
+
+const pathToReact = fileURLToPath(new URL('../../../client/build/index.html', import.meta.url));
 
 export const register = async (req, res) => {
-  const obtainedUserData = req.body;
-  //delete password Confirm from object send DB
-  delete obtainedUserData.passwordConfirm;
-  obtainedUserData.password = await createHash(obtainedUserData.password);
-  // eslint-disable-next-line camelcase
-  obtainedUserData.confirm_user = await createHash(
-    obtainedUserData.confirm_user,
-  );
-  // eslint-disable-next-line camelcase
-  const activeUser = await user.selectFirst({
-    email: obtainedUserData.email,
-    is_active: 1,
-  });
-  if (activeUser) {
-    res.writeHead(404, { 'Content-Type': 'application/json' });
-    res.end(JSON.stringify('User already active'));
-  } else {
-    await user.save(obtainedUserData);
-    res.writeHead(200, { 'Content-Type': 'application/json' });
-    res.end(JSON.stringify(obtainedUserData));
-  }
+    const obtainedUserData = req.body;
+    //delete password Confirm from object send DB
+    delete obtainedUserData.passwordConfirm;
+    obtainedUserData.password = await createHash(obtainedUserData.password);
+    // eslint-disable-next-line camelcase
+    obtainedUserData.confirm_user = await createHash(
+        obtainedUserData.confirm_user,
+    );
+    // eslint-disable-next-line camelcase
+    const activeUser = await user.selectFirst({
+        email: obtainedUserData.email,
+        is_active: 1,
+    });
+    if (activeUser) {
+        res.writeHead(404, {'Content-Type': 'application/json'});
+        res.end(JSON.stringify('User already active'));
+    } else {
+        await user.save(obtainedUserData);
+        res.writeHead(200, {'Content-Type': 'application/json'});
+        res.end(JSON.stringify('Success register'));
+    }
 };
 
 export const login = async (req, res) => {
-  const obtainedUserData = req.body;
-  const authUser = await user.selectFirst({ email: obtainedUserData.email });
-  if (authUser) {
-    // check password, generate token, successfully logged in
-    const accessUser = await comparePass(
-      obtainedUserData.password,
-      authUser.password,
-    );
-    if (accessUser) {
-      const accessToken = createToken({ id: authUser.id });
-      res
-        .status(200)
-        .cookie('access_token', accessToken, {
-          httpOnly: true,
-        })
-        .json('Success login');
+    const obtainedUserData = req.body;
+    const authUser = await user.selectFirst({email: obtainedUserData.email});
+    if (authUser) {
+        // check password, generate token, successfully logged in
+        const accessUser = await comparePass(
+            obtainedUserData.password,
+            authUser.password,
+        );
+        if (accessUser) {
+            const accessToken = createToken({id: authUser.id});
+            res
+                .status(200)
+                .cookie('access_token', accessToken, {
+                    httpOnly: true,
+                })
+                .json('Success login');
+        } else {
+            res.writeHead(404, {'Content-Type': 'application/json'});
+            res.end(JSON.stringify('Password wrong'));
+        }
     } else {
-      res.writeHead(404, { 'Content-Type': 'application/json' });
-      res.end(JSON.stringify('Password wrong'));
+        res.writeHead(404, {'Content-Type': 'application/json'});
+        res.end(JSON.stringify('User not registered'));
     }
-  } else {
-    res.writeHead(404, { 'Content-Type': 'application/json' });
-    res.end(JSON.stringify('User not registered'));
-  }
 };
+
+export const sendFile = (req, res) => {
+    res.sendFile(pathToReact);
+}
+
+export const logout = (req, res) => {
+    res.clearCookie('access_token');
+    res.redirect('/signIn');
+}
--- app/routes/userRouter.js
+++ app/routes/userRouter.js
@@ -1,16 +1,31 @@
 import passport from 'passport';
-import { login, register } from '../Http/Controllers/user.js';
+import { login, logout, register, sendFile } from '../Http/Controllers/user.js';
 import { Router } from 'express';
 import {
   registerValidate,
   loginValidate,
   validate,
 } from '../../lib/validate/user.js';
+import { fileURLToPath } from 'url';
+
+/*Absolute paths for react*/
+const pathToReactjs = fileURLToPath(new URL('../../client/build/main.js', import.meta.url));
 
 export const router = Router();
+/*Routers without Auth user*/
+router.get('/main.js', (req, res) => {
+  res.sendFile(pathToReactjs);
+});
+router.get('/signIn', sendFile);
+router.get('/signUp', sendFile);
 
-router.get('*', passport.authenticate('jwt', { session: false }), (req, res) =>
-  res.send('HOME'),
-);
 router.post('/user/login', loginValidate, validate, login);
 router.post('/user/registration', registerValidate, validate, register);
+
+router.get('/logout', logout);
+/*Privat Route*/
+router.get('*', passport.authenticate('jwt', {
+      session: false,
+      failureRedirect: '/signIn',
+    }), sendFile,
+);
