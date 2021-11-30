2e74cec36cb8c96fd0f2e7ee8da713506252f903
Anastiuk <dse6023@gmail.com>
2021-11-30 22:07:09 +0200

    Fix routes to  protect '/' all another routes is public. Add static connect builde dir

--- app/routes/userRouter.js
+++ app/routes/userRouter.js
@@ -6,26 +6,18 @@ import {
   loginValidate,
   validate,
 } from '../../lib/validate/user.js';
-import { fileURLToPath } from 'url';
-
-/*Absolute paths for react*/
-const pathToReactjs = fileURLToPath(new URL('../../client/build/main.js', import.meta.url));
 
 export const router = Router();
 /*Routers without Auth user*/
-router.get('/main.js', (req, res) => {
-  res.sendFile(pathToReactjs);
-});
-router.get('/signIn', sendFile);
-router.get('/signUp', sendFile);
+router.get('*', sendFile);
 
 router.post('/user/login', loginValidate, validate, login);
 router.post('/user/registration', registerValidate, validate, register);
 
 router.get('/logout', logout);
 /*Privat Route*/
-router.get('*', passport.authenticate('jwt', {
-      session: false,
-      failureRedirect: '/signIn',
-    }), sendFile,
+router.get('/', passport.authenticate('jwt', {
+    session: false,
+    failureRedirect: '/signIn',
+  }), sendFile,
 );
