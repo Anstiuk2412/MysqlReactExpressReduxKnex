7b5526bd3033d9cb57d9d78e90b1eeff272f8ba2
Daniil Anastiuk <dse6023@gmail.com>
2022-01-10 21:42:28 +0200

    Rewrited file controller

--- app/Http/Controllers/files.js
+++ app/Http/Controllers/files.js
@@ -5,13 +5,12 @@ export const filesAndFoldersAtFolder = async (req, res) => {
   // * get User id
   const userId = req.user.user_id;
   // * get folder id
-  const folderId = req.params.folder_id;
-  const mainFolderInfo = await folders.selectFirst({
+  let mainFolder = await folders.selectFirst({
     // eslint-disable-next-line camelcase
     user_id: userId,
-    id: folderId,
+    id: req.params.folder_id,
   });
-  if (folderId === 'undefined') {
+  if (!mainFolder) {
     // * If user at Home folder
     const homeFolderId = await folders.selectFirst({
       user_id: userId,
@@ -40,30 +39,26 @@ export const filesAndFoldersAtFolder = async (req, res) => {
     }
     return;
   }
-  // * If user not at Home folder get all Files
-  const { userFiles, childFolders } = await openFolder(userId, folderId);
-  if (userFiles[0]) {
-    res.writeHead(200, { 'Content-Type': 'application/json' });
-    res.end(
-      JSON.stringify({
-        data: {
-          files: userFiles,
-          folders: childFolders,
-          parent_id: mainFolderInfo.parent_id,
-        },
-      }),
-    );
-  } else {
-    res.writeHead(200, { 'Content-Type': 'application/json' });
-    res.end(
-      JSON.stringify({
-        data: {
-          files: userFiles,
-          folders: childFolders,
-          parent_id: mainFolderInfo.parent_id,
-          message: ['The folder is empty'],
-        },
-      }),
-    );
+  // ! This query solves the problem with URL.
+  // ! If the user goes back to the home folder he should see this '/'
+  // ! and not this '/folder/${folder_id}'
+  const parentFolder = await folders.selectFirst({
+    user_id: userId,
+    id: mainFolder.parent_id,
+  });
+  if (parentFolder && !parentFolder.parent_id) {
+    mainFolder.parent_id = null;
   }
+  // * If user not at Home folder get all Files
+  const { userFiles, childFolders } = await openFolder(userId, mainFolder.id);
+  res.writeHead(200, { 'Content-Type': 'application/json' });
+  res.end(
+    JSON.stringify({
+      data: {
+        files: userFiles,
+        folders: childFolders,
+        parent_id: mainFolder.parent_id,
+      },
+    }),
+  );
 };
