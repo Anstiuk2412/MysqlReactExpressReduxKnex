585592e84b0f03a6b07ae751b0c6be0eee897f24
Daniil Anastiuk <dse6023@gmail.com>
2022-01-11 22:18:48 +0200

    ÃFixed and optimized knex query in modelHelper

--- database/models/modelsHelper.js
+++ database/models/modelsHelper.js
@@ -1,27 +1,23 @@
 import { myKnex } from '../knexfile.js';
 
 export const where = (table, query) => {
-  return myKnex(table).select().where(query);
+  return myKnex(table).select().where(query).then();
 };
 
 export const insert = (table, conditions) => {
-  return myKnex(table).insert(conditions);
+  return myKnex(table).insert(conditions).then();
 };
 
 export const update = (table, conditions) => {
-  return myKnex(table).where({ id: conditions.id }).update(conditions);
+  return myKnex(table).where({ id: conditions.id }).update(conditions).then();
 };
 
 export const selectFirst = (table, conditions) => {
   return myKnex(table).select().where(conditions).first().then();
 };
 
-export const selectAll = (table, conditions) => {
-  return myKnex(table).select().where(conditions);
-};
-
 export const upsert = (table, data, key, mergeValue) => {
-  return myKnex(table).insert(data).onConflict(key).merge([mergeValue]);
+  return myKnex(table).insert(data).onConflict(key).merge([mergeValue]).then();
 };
 
 export const save = (table, conditions, key, mergeValue) => {
