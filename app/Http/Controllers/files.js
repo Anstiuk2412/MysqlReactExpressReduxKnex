0dd5294b7fb55fd838bcf0948dec0feada7caba8
Daniil Anastiuk <dse6023@gmail.com>
2022-01-16 17:01:29 +0200

    Revert "Rewrited file Controller without async. And decompose folderId"
    This reverts commit 4c6a96089711d47573fc40e0086c3113debe517e.

--- app/Http/Controllers/files.js
+++ app/Http/Controllers/files.js
@@ -1,39 +1,57 @@
 import { openFolder } from '../../../lib/helper/workWithFilesAndFolders/openFolder.js';
 import { folders } from '../../../database/models/folder.js';
 
-export const filesAndFoldersAtFolder = (req, res) => {
+export const filesAndFoldersAtFolder = async (req, res) => {
   // * get User id
   const userId = req.user.user_id;
   // * get folder id
-  const folderId =
-    req.params.folder_id === 'undefined'
-      ? { parent_id: null }
-      : { id: req.params.folder_id };
-  const getFolderConds = {
-    user_id: userId, // * always exists
-    ...folderId,
-  };
-  // * get Folder info
-  folders.selectFirst(getFolderConds).then((mainFolder) => {
-    // * get files at mainFolder and folder at mainFolder
-    openFolder(userId, mainFolder.id).then(({ userFiles, childFolders }) => {
-      if (userFiles[0]) {
-        res.writeHead(200, { 'Content-Type': 'application/json' });
-        res.end(
-          JSON.stringify({ data: { files: userFiles, folders: childFolders } }),
-        );
-      } else {
-        res.writeHead(200, { 'Content-Type': 'application/json' });
-        res.end(
-          JSON.stringify({
-            data: {
-              files: userFiles,
-              folders: childFolders,
-              parent_id: mainFolder.parent_id,
-            },
-          }),
-        );
-      }
+  const folderId = req.params.folder_id;
+  if (folderId === 'undefined') {
+    // * If user at Home folder
+    const homeFolderId = await folders.selectFirst({
+      user_id: userId,
+      parent_id: null,
     });
-  });
+    const { userFiles, childFolders } = await openFolder(
+      userId,
+      homeFolderId.id,
+    );
+    if (userFiles[0]) {
+      res.writeHead(200, { 'Content-Type': 'application/json' });
+      res.end(
+        JSON.stringify({ data: { files: userFiles, folders: childFolders } }),
+      );
+    } else {
+      res.writeHead(200, { 'Content-Type': 'application/json' });
+      res.end(
+        JSON.stringify({
+          data: {
+            files: userFiles,
+            folders: childFolders,
+            message: ['The folder is empty'],
+          },
+        }),
+      );
+    }
+    return;
+  }
+  // * If user not at Home folder get all Files
+  const { userFiles, childFolders } = await openFolder(userId, folderId);
+  if (userFiles[0]) {
+    res.writeHead(200, { 'Content-Type': 'application/json' });
+    res.end(
+      JSON.stringify({ data: { files: userFiles, folders: childFolders } }),
+    );
+  } else {
+    res.writeHead(200, { 'Content-Type': 'application/json' });
+    res.end(
+      JSON.stringify({
+        data: {
+          files: userFiles,
+          folders: childFolders,
+          message: ['The folder is empty'],
+        },
+      }),
+    );
+  }
 };
