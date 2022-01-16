121725979d710512f8511ee5c74064a614f1ecae
Daniil Anastiuk <dse6023@gmail.com>
2022-01-16 17:07:38 +0200

    Rewrited file Controller without async. And decompose folderId

--- app/Http/Controllers/files.js
+++ app/Http/Controllers/files.js
@@ -1,41 +1,39 @@
 import { openFolder } from '../../../lib/helper/workWithFilesAndFolders/openFolder.js';
 import { folders } from '../../../database/models/folder.js';
 
-export const filesAndFoldersAtFolder = async (req, res) => {
-  // * Get User id
+export const filesAndFoldersAtFolder = (req, res) => {
+  // * get User id
   const userId = req.user.user_id;
-  // * Get values of main folder
-  let mainFolder = await folders.selectTwoWhere(
-    {
-      // * If it's main folder
-      user_id: userId,
-      parent_id: null,
-    },
-    {
-      // * If it's child folder
-      user_id: userId,
-      id: req.params.folder_id,
-    },
-  );
-  // ! This query solves the problem with URL.
-  // ! If the user goes back to the home folder he should see this '/'
-  // ! and not this '/folder/${folder_id}'
-  const parentFolder = await folders.selectFirst({
-    user_id: userId,
-    id: mainFolder.parent_id,
+  // * get folder id
+  const folderId =
+    req.params.folder_id === 'undefined'
+      ? { parent_id: null }
+      : { id: req.params.folder_id };
+  const getFolderConds = {
+    user_id: userId, // * always exists
+    ...folderId,
+  };
+  // * get Folder info
+  folders.selectFirst(getFolderConds).then((mainFolder) => {
+    // * get files at mainFolder and folder at mainFolder
+    openFolder(userId, mainFolder.id).then(({ userFiles, childFolders }) => {
+      if (userFiles[0]) {
+        res.writeHead(200, { 'Content-Type': 'application/json' });
+        res.end(
+          JSON.stringify({ data: { files: userFiles, folders: childFolders } }),
+        );
+      } else {
+        res.writeHead(200, { 'Content-Type': 'application/json' });
+        res.end(
+          JSON.stringify({
+            data: {
+              files: userFiles,
+              folders: childFolders,
+              parent_id: mainFolder.parent_id,
+            },
+          }),
+        );
+      }
+    });
   });
-  if (parentFolder && !parentFolder.parent_id) {
-    mainFolder.parent_id = null;
-  }
-  const { userFiles, childFolders } = await openFolder(userId, mainFolder.id);
-  res.writeHead(200, { 'Content-Type': 'application/json' });
-  res.end(
-    JSON.stringify({
-      data: {
-        files: userFiles,
-        folders: childFolders,
-        parent_id: mainFolder.parent_id,
-      },
-    }),
-  );
 };
