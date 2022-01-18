47a01020bb98fc1153f8886f656ebefd5970cd8c
Daniil Anastiuk <dse6023@gmail.com>
2022-01-18 15:29:25 +0200

    Unusing async at model

--- database/models/modelsHelper.js
+++ database/models/modelsHelper.js
@@ -1,23 +1,23 @@
 import { myKnex } from '../knexfile.js';
 
 export const where = (table, query) => {
-  return myKnex(table).select().where(query).then();
+  return myKnex(table).select().where(query);
 };
 
 export const insert = (table, conditions) => {
-  return myKnex(table).insert(conditions).then();
+  return myKnex(table).insert(conditions);
 };
 
 export const update = (table, conditions) => {
-  return myKnex(table).where({ id: conditions.id }).update(conditions).then();
+  return myKnex(table).where({ id: conditions.id }).update(conditions);
 };
 
 export const selectFirst = (table, conditions) => {
-  return myKnex(table).select().where(conditions).first().then();
+  return myKnex(table).select().where(conditions).first();
 };
 
 export const upsert = (table, data, key, mergeValue) => {
-  return myKnex(table).insert(data).onConflict(key).merge([mergeValue]).then();
+  return myKnex(table).insert(data).onConflict(key).merge([mergeValue]);
 };
 
 export const save = (table, conditions, key, mergeValue) => {
